52768661d53e038164e5cd543c68321caf2e00aa
Kevin-1DOS <kevinasdw124@gmail.com>
2026-07-22 12:03:47 +0700

    first commit: sistem sekolah v2

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Manejemen Siswa
+Route::name("students.")->prefix('/students')->group(function() {
+    Route::get('/', [StudentController::class, 'index'])->name("index");
+
+    Route::get('/{id}', [StudentController::class, 'show'])->name("show");
+});
+
+
+
